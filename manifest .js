8b1b92c48a3345642eb69115ceaae38f79931d50
Thein-maung <59556712+Thein-maung@users.noreply.github.com>
2025-11-15 12:50:13 +0630

    Add files via upload

---
+++ manifest .js
@@ -0,0 +1,12 @@
+{
+  "name": "Entangle Chat-2",
+  "short_name": "Entangle",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#0f2027",
+  "theme_color": "#00bcd4",
+  "icons": [
+    { "src": "icon-192.png", "sizes": "192x192", "type": "image/png" },
+    { "src": "icon-512.png", "sizes": "512x512", "type": "image/png" }
+  ]
+}
